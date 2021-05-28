fc91d36a87d5663077298049034c3eaf8040bf70
Feodor Zharov <zhar-fyodor@yandex.ru>
2021-05-28 12:48:04 +0300

    back to HttpServletRequest

--- src/main/java/frg/heroku/test/controller/AliceRestController.java
+++ src/main/java/frg/heroku/test/controller/AliceRestController.java
@@ -29,19 +29,23 @@ public class AliceRestController {
     }
 
     @PostMapping("")
-    public AliceResponse repeat(@RequestBody AliceRequest req, ServerHttpRequest request) {
+    public AliceResponse repeat(@RequestBody AliceRequest req, HttpServletRequest request) { //, ServerHttpRequest request) {
         log.info("---------------");
+
+        /*
         log.info("1) request.getRemoteAddress().getHostName() = " + request.getRemoteAddress().getHostName());
         log.info("2) request.getRemoteAddress().getHostString()= " + request.getRemoteAddress().getHostString());
         log.info("3) request.getRemoteAddress().getAddress() = " + request.getRemoteAddress().getAddress());
         log.info("4) request.getRemoteAddress().getPort()= " + request.getRemoteAddress().getPort());
-        /*
+         */
+
+
         log.info("1) request.getRequestURI().toString() = " + request.getRequestURI());
         log.info("2) request.getRequestURL().toString() = " + request.getRequestURL().toString());
         log.info("3) request.getRemoteAddr().toString() = " + request.getRemoteAddr());
         log.info("4) request.getRemoteHost().toString() = " + request.getRemoteHost());
         log.info("5) request.getRemotePort().toString() = " + request.getRemotePort());
-         */
+
         log.info("---------------");
         return aliceService.call(req);
     }
