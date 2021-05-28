3e33919195191e0bdd4d12371f748746462a5519
Feodor Zharov <zhar-fyodor@yandex.ru>
2021-05-28 12:40:00 +0300

    ServerHttpRequest info

--- src/main/java/frg/heroku/test/controller/AliceRestController.java
+++ src/main/java/frg/heroku/test/controller/AliceRestController.java
@@ -8,6 +8,7 @@ import frg.heroku.test.payload.res.AliceResponse;
 import frg.heroku.test.service.AliceService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,10 +29,19 @@ public class AliceRestController {
     }
 
     @PostMapping("")
-    public AliceResponse repeat(@RequestBody AliceRequest req, HttpServletRequest request) {
+    public AliceResponse repeat(@RequestBody AliceRequest req, ServerHttpRequest request) {
         log.info("---------------");
+        log.info("1) request.getRemoteAddress().getHostName() = " + request.getRemoteAddress().getHostName());
+        log.info("2) request.getRemoteAddress().getHostString()= " + request.getRemoteAddress().getHostString());
+        log.info("3) request.getRemoteAddress().getAddress() = " + request.getRemoteAddress().getAddress());
+        log.info("4) request.getRemoteAddress().getPort()= " + request.getRemoteAddress().getPort());
+        /*
         log.info("1) request.getRequestURI().toString() = " + request.getRequestURI());
         log.info("2) request.getRequestURL().toString() = " + request.getRequestURL().toString());
+        log.info("3) request.getRemoteAddr().toString() = " + request.getRemoteAddr());
+        log.info("4) request.getRemoteHost().toString() = " + request.getRemoteHost());
+        log.info("5) request.getRemotePort().toString() = " + request.getRemotePort());
+         */
         log.info("---------------");
         return aliceService.call(req);
     }
